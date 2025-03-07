faa3eb819ee216e91a0404999b95209389c84e8b
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-07 14:49:03 +0530

    feat: Add mutation for updating user health details in RTK Query

--- src/redux/api/caloriesApi.ts
+++ src/redux/api/caloriesApi.ts
@@ -2,6 +2,14 @@ import { apiSlice } from "./EntryApiSlice";
 
 export const chatApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    userHealthDetail: builder.mutation({
+      query: (data) => ({
+        url: "/app/update-userdetails",
+        method: "POST",
+        body: data,
+      }),
+    }),
+
     searchFoodItems: builder.mutation({
       query: (data) => ({
         url: "/app/serach-food",
