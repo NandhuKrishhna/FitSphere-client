7cfcb0eff39750045d315544a65c8ca5d0a8e470
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-09 00:29:02 +0530

    feat : Give routes for all the elements

--- src/components/App/Navigation.tsx
+++ src/components/App/Navigation.tsx
@@ -1,24 +1,37 @@
-import { Home, BookOpen, Coffee, Settings } from "lucide-react";
+import { Link } from "react-router-dom";
+import { BookOpen, House, MessageCircle, UserRoundPlus, Video, Wallet } from "lucide-react";
 
 export default function Navigation() {
   return (
-    <nav className="flex justify-around py-2 border-b border-gray-800">
-      <button className="flex flex-col items-center p-2 text-purple-400">
-        <Home className="w-5 h-5" />
-        <span className="text-xs mt-1">Dashboard</span>
-      </button>
-      <button className="flex flex-col items-center p-2 text-gray-500">
+    <nav className="flex justify-around py-2 border-b border-gray-800 bg-[#1e1e30]">
+      <Link to="/home" className="flex flex-col items-center p-2 text-gray-500">
+        <House className="w-5 h-5" />
+        <span className="text-xs mt-1">Home</span>
+      </Link>
+
+      <Link to="/doctors/all" className="flex flex-col items-center p-2 text-purple-400">
+        <UserRoundPlus className="w-5 h-5" />
+        <span className="text-xs mt-1">Doctors</span>
+      </Link>
+
+      <Link to="/recipes" className="flex flex-col items-center p-2 text-gray-500">
         <BookOpen className="w-5 h-5" />
         <span className="text-xs mt-1">Recipes</span>
-      </button>
-      <button className="flex flex-col items-center p-2 text-gray-500">
-        <Coffee className="w-5 h-5" />
-        <span className="text-xs mt-1">Meals</span>
-      </button>
-      <button className="flex flex-col items-center p-2 text-gray-500">
-        <Settings className="w-5 h-5" />
-        <span className="text-xs mt-1">Options</span>
-      </button>
+      </Link>
+
+      <Link to="/chat" className="flex flex-col items-center p-2 text-gray-500">
+        <MessageCircle className="w-5 h-5" />
+        <span className="text-xs mt-1">Chat</span>
+      </Link>
+
+      <Link to="/wallet" className="flex flex-col items-center p-2 text-gray-500">
+        <Wallet className="w-5 h-5" />
+        <span className="text-xs mt-1">Wallet</span>
+      </Link>
+      <Link to="/create-meet" className="flex flex-col items-center p-2 text-gray-500">
+        <Video className="w-5 h-5" />
+        <span className="text-xs mt-1">Meet</span>
+      </Link>
     </nav>
   );
 }
