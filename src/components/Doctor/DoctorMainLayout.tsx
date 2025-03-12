2da081e2f3be78eb6b45dce6a9547bfeb8ce9878
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-12 11:49:54 +0530

    feat(DoctorMainLayout): refactor layout to use Header and Navigation components

--- src/components/Doctor/DoctorMainLayout.tsx
+++ src/components/Doctor/DoctorMainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
-import DoctorHeader from "./DoctorHeader";
+import Header from "../App/Header";
+import Navigation from "../App/Navigation";
 
 const DoctorMainLayout = () => {
   return (
-    <div className="bg-gradient-to-br from-gray-500 to-black min-h-screen flex flex-col">
-      <DoctorHeader />
+    <div className="min-h-screen bg-[#121212] text-white flex flex-col">
+      <Header />
+      <Navigation />
       <main className="flex-1 overflow-hidden">
         <div className="h-full">
           <Outlet />
