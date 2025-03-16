2944938a90a40815d0d19f652dccf6b0cde5fede
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-16 13:33:51 +0530

    feat(hooks): enhance food deletion handling with loading state management

--- src/hooks/App/useDeleteFood.ts
+++ src/hooks/App/useDeleteFood.ts
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { useDeleteFoodLogMutation } from "@/redux/api/caloriesApi";
 import toast from "react-hot-toast";
 import { ErrorResponse } from "../LoginHook";
 
 const useDeleteFood = () => {
-  const [deleteFoodLog, { isLoading }] = useDeleteFoodLogMutation();
+  const [deleteFoodLog] = useDeleteFoodLogMutation();
+  const [loadingItems, setLoadingItems] = useState<{ [key: string]: boolean }>({});
 
   const handleDeleteFood = async (foodId?: string, date?: string) => {
+    if (!foodId) return;
+    
+    setLoadingItems((prev) => ({ ...prev, [foodId]: true }));
+
     try {
       const response = await deleteFoodLog({ foodId, date }).unwrap();
       toast.success(response.message);
@@ -13,11 +19,14 @@ const useDeleteFood = () => {
       const err = error as ErrorResponse;
       if (err?.data?.message) return toast.error(err.data.message);
       toast.error("An unexpected error occurred. Please try again.");
+    } finally {
+      setLoadingItems((prev) => ({ ...prev, [foodId]: false }));
     }
   };
+
   return {
     handleDeleteFood,
-    isLoading,
+    loadingItems, 
   };
 };
 
