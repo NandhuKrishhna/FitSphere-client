c807d5e18586e3ed7bcc222179bc80dd55d0ca67
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-17 23:34:42 +0530

    feat(conversation): implement custom hook for creating conversations with error handling

---
+++ src/hooks/App/useCreateConversation.ts
@@ -0,0 +1,21 @@
+import { useCreateConversationMutation } from "@/redux/api/chatApi";
+import { createConversationProps } from "@/utils/DoctorDetailsPageUtils";
+import toast from "react-hot-toast";
+import { ErrorResponse } from "react-router-dom";
+
+const useCreateConversation = () => {
+  const [createConversation] = useCreateConversationMutation();
+   const createConversationHandler = async ({senderId, receiverId}:createConversationProps) => {
+       try {
+           await createConversation({ senderId, receiverId }).unwrap();
+       }catch(error){
+          console.log(error);
+          const err = error as ErrorResponse;
+          if(err.data.message) return toast.error(err.data.message);
+          toast.error("An unexpected error occurred. Please try again.");
+       }
+   }
+   return {createConversationHandler}
+}
+
+export default useCreateConversation;
