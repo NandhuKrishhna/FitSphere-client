ee39fe992c5500b531e484caa231cc5d64dc3af3
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-18 20:17:54 +0530

    feat(DoctorDetailsPageUtils): enhance handleOptionClick to support conversation creation with current user ID

--- src/utils/DoctorDetailsPageUtils.ts
+++ src/utils/DoctorDetailsPageUtils.ts
@@ -1,16 +1,23 @@
+
 import { setSelectedUser } from "@/redux/slice/socket.ioSlice";
 import { AppDispatch } from "@/redux/store";
 import { SelectedUser } from "@/types/ChatTypes";
 import { NavigateFunction } from "react-router-dom";
-
+export type createConversationProps = {
+  senderId: string,
+  receiverId : string
+}
 export const handleOptionClick = (
   option: string,
   navigate: NavigateFunction,
   dispatch: AppDispatch,
-  user: SelectedUser
+  user: SelectedUser,
+  currentUserId: string | undefined,
+  createConversationHandler: ({senderId, receiverId}:createConversationProps) => Promise<string | undefined>
 ) => {
   if (option === "Chat") {
     dispatch(setSelectedUser(user));
+    createConversationHandler({senderId: currentUserId!, receiverId: user.doctorDetails._id!});
     navigate("/chat");
   } else if (option === "Video") {
     navigate("/video");
