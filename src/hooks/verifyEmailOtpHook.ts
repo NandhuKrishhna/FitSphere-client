e30c864d76d27fd08f6d8eaf08dc35ce9bd1952c
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-19 02:32:17 +0530

    feat(verifyEmailOtpHook): enhance email verification flow with user data handling and navigation improvements

--- src/hooks/verifyEmailOtpHook.ts
+++ src/hooks/verifyEmailOtpHook.ts
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useVerfiyEmailMutation } from "../redux/api/apiSlice";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "@/redux/slice/Auth_Slice";
 interface ErrorResponse {
   data: {
     errors?: Array<{ path: string; message: string }>;
@@ -13,7 +15,7 @@ const useVerificationCodeHook = () => {
   const [otp, setOtp] = useState<string[]>(new Array(6).fill(""));
   const navigate = useNavigate();
   const [verfiyEmail, { isLoading }] = useVerfiyEmailMutation();
-
+  const dispatch = useDispatch()
   const validateForm = () => {
     const otpString = otp.join("");
     console.log(otpString);
@@ -24,15 +26,21 @@ const useVerificationCodeHook = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const success = validateForm();
+  
     if (success === true) {
       try {
         const userId = localStorage.getItem("userId");
         const res = await verfiyEmail({ code: otp.join(""), userId: userId }).unwrap();
-        console.log(res);
+        const userDetails = JSON.parse(localStorage.getItem("userData") || "{}");
         toast.success(res.message);
         setOtp(new Array(6).fill(""));
         localStorage.removeItem("userId");
-        navigate("/age");
+        localStorage.removeItem("userData");
+        localStorage.setItem("signupInProgress", "true");
+        navigate("/age", { replace: true });
+        setTimeout(() => {
+          dispatch(setCredentials({ ...userDetails }));
+        }, 500); 
       } catch (err) {
         console.log(err);
         const error = err as ErrorResponse;
