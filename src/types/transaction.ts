097b5a08dc58e07d7cb725ef86aea5f90b9d784f
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-18 20:17:49 +0530

    feat(transaction): add TransactionQueryParams and Transaction types for improved transaction handling

---
+++ src/types/transaction.ts
@@ -0,0 +1,38 @@
+export type TransactionQueryParams ={
+  page?: number
+  limit?: number
+  search?: string
+  type?: string
+  status?: string
+  paymentType?: string
+  sortBy?: string
+  sortOrder?: "asc" | "desc"
+  }
+
+ export  type Transaction = {
+    _id: string;
+    amount: number;
+    currency: string;
+    type: "credit" | "debit" |"failed"; 
+    method: "wallet" | "razorpay"; 
+    paymentType: "slot_booking" | "subscription"; 
+    status: "pending" | "success" | "failed"; 
+    createdAt: string; 
+  
+    fromDetails: {
+      _id: string;
+      name: string;
+      email: string;
+      profilePicture: string;
+    };
+  
+    toDetails: {
+      _id: string;
+      name: string;
+      email: string;
+      profilePicture: string;
+    };
+  };
+  
+ export  type TransactionResponse = Transaction[];
+  
