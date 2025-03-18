661e22ad4e5fae5005c966bf788aa2080fa055de
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-19 02:29:18 +0530

    fix(useTimeFormatter): clean up formatting logic for better readability

--- src/utils/useTimeFormatter.ts
+++ src/utils/useTimeFormatter.ts
@@ -1,8 +1,8 @@
 import { format, toZonedTime } from "date-fns-tz";
 
 export const formatToIndianTime = (utcTime: string) => {
-  const zonedDate = toZonedTime(utcTime, "UTC"); // Keep it in UTC
-  return format(zonedDate, "hh:mm a"); // Format without conversion
+  const zonedDate = toZonedTime(utcTime, "UTC");
+  return format(zonedDate, "hh:mm a");
 };
 
 export const formatDate = (dateString: string): string => {
