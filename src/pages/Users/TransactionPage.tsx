76123af010476c6a4f16e0991354c26df9ccac06
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-20 01:11:23 +0530

    feat(transactions): conditionally render Header and Navigation based on user role

--- src/pages/Users/TransactionPage.tsx
+++ src/pages/Users/TransactionPage.tsx
@@ -1,13 +1,20 @@
 import Header from "@/components/App/Header";
 import Navigation from "@/components/App/Navigation";
 import TransactionTable from "@/components/App/transaction-table";
+import { selectCurrentUser } from "@/redux/slice/Auth_Slice";
+import { useSelector } from "react-redux";
 
 
 export default function TransactionsPage() {
+  const role = useSelector(selectCurrentUser)?.role
   return (
     <div className="min-h-screen bg-black">
-    <Header/>
-    <Navigation/>
+      {role === "user" &&(
+        <>
+            <Header/>
+            <Navigation/>
+        </>
+      )}
       <div className="pt-10">
 
       <TransactionTable />
