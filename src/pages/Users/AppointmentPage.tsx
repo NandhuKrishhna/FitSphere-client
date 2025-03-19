102d163618787b4e763dbc9ce73db09a58d13e6c
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-20 01:11:28 +0530

    feat(appointment): conditionally render Header and Navigation based on user role

--- src/pages/Users/AppointmentPage.tsx
+++ src/pages/Users/AppointmentPage.tsx
@@ -1,13 +1,21 @@
 import AppointmentTable from "@/components/App/Appointment-table";
 import Header from "@/components/App/Header";
 import Navigation from "@/components/App/Navigation";
+import { selectCurrentUser } from "@/redux/slice/Auth_Slice";
+import { useSelector } from "react-redux";
 
 
 export default function AppointmentPage() {
+  const role = useSelector(selectCurrentUser)?.role
   return (
     <div className="min-h-screen bg-black">
-    <Header/>
-    <Navigation/>
+      {role === "user" &&(
+        <>
+            <Header/>
+            <Navigation/>
+        </>
+      )}
+
       <div className="pt-10">
 
       <AppointmentTable />
