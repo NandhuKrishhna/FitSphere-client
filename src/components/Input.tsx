f50176da0e3cabe5ea3e4108ded1021b6a4ab388
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-21 03:32:03 +0530

    feat(input): add defaultValue prop to InputField component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,9 +7,10 @@ interface InputFieldProps {
   placeholder: string;
   icon?: React.ReactNode;
   register: UseFormRegisterReturn;
+  defaultValue?: string;
 }
 
-const InputField: FC<InputFieldProps> = ({ id, type = "text", placeholder, icon, register }) => {
+const InputField: FC<InputFieldProps> = ({ id, type = "text", placeholder, icon, register, defaultValue }) => {
   return (
     <div className="space-y-2">
       <label htmlFor={id} className="text-sm font-medium text-zinc-300">
@@ -21,6 +22,7 @@ const InputField: FC<InputFieldProps> = ({ id, type = "text", placeholder, icon,
           type={type}
           {...register}
           placeholder={placeholder}
+          defaultValue={defaultValue}
           className="w-full py-2 px-3 bg-zinc-800 rounded-md text-white placeholder-zinc-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 caret-white"
         />
         {icon && <div className="absolute right-3 top-1/2 transform -translate-y-1/2 text-zinc-500">{icon}</div>}
