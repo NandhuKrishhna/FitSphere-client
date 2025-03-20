067925bee4fb41bd688c9350f563eedf1bff09be
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-21 03:42:48 +0530

    feat(users): integrate Google login option in LoginPage component

--- src/pages/Users/LoginPage.tsx
+++ src/pages/Users/LoginPage.tsx
@@ -1,8 +1,10 @@
 import AuthLayout from "@/components/App/AuthLayout";
 import useLoginHook from "@/hooks/LoginHook";
+import useGoogleLoginHook from "@/hooks/useGoogleLogin";
 
 export default function LoginPage() {
   const { register, isLoading, errors, handleSubmit } = useLoginHook();
+  const {googleLogin} = useGoogleLoginHook()
 
   return (
     <AuthLayout
@@ -18,6 +20,8 @@ export default function LoginPage() {
       footerLinkPath="/signup"
       submitButtonText="Login"
       forgotPasswordURL="/forgot-password"
+      googleLoginOption={true}
+      handleGoogleLogin={googleLogin}
     />
   );
 }
