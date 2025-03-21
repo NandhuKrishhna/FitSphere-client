c65a6cf1a8e7bd7ef6792dbb7135f729dbadfd80
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-22 02:01:58 +0530

    feat(BlockUnblockUserHook): enhance blocking/unblocking functionality with role support and loading states

--- src/hooks/Admin/Block_UnBlockUserHook.tsx
+++ src/hooks/Admin/Block_UnBlockUserHook.tsx
@@ -1,5 +1,6 @@
 import toast from "react-hot-toast";
 import { useBlockUsersMutation, useUnblockUsersMutation } from "../../redux/api/adminApi";
+import React from "react";
 
  type ErrorResponse = {
   data :{
@@ -8,37 +9,46 @@ import { useBlockUsersMutation, useUnblockUsersMutation } from "../../redux/api/
   status:number
  }
 export default function useBlockUnblockUser() {
-  const [blockUser, { isLoading: isBlocking }] = useBlockUsersMutation();
-  const [unblockUser, { isLoading: isUnblocking }] = useUnblockUsersMutation();
+  const [blockUser ] = useBlockUsersMutation();
+  const [unblockUser] = useUnblockUsersMutation();
+  const [isblocking , setIsBlocking] = React.useState<{ [key: string]: boolean }>({})
+  const [isunblocking , setIsUnBlocking] = React.useState<{ [key: string]: boolean }>({})
 
-  const handleBlock = async (id: string) => {
+  const handleBlock = async (id: string , role : string) => {
+    setIsBlocking((prev) => ({ ...prev, [id]: true }));
     try {
-      const response = await blockUser({ id }).unwrap();
+      const response = await blockUser({ id , role}).unwrap();
       toast.success("User blocked successfully")
       return response;
     } catch (error) {
       const err  = error as ErrorResponse
       if(err?.data?.message)
        toast.error(err.data.message)
+    }finally{
+      setIsBlocking((prev) => ({ ...prev, [id]: false }));
     }
   };
 
-  const handleUnblock = async (id: string) => {
+  const handleUnblock = async (id: string, role : string) => {
+    setIsUnBlocking((prev) => ({ ...prev, [id]: true }));
     try {
-      const response = await unblockUser({ id }).unwrap();
+      const response = await unblockUser({ id , role}).unwrap();
       toast.success("User unblocked successfully")
       return response;
     } catch (error) {
       const err  = error as ErrorResponse
       if(err?.data?.message)
        toast.error(err.data.message)
+    }finally{
+      setIsUnBlocking((prev) => ({ ...prev, [id]: false }));
     }
   };
 
   return {
     handleBlock,
     handleUnblock,
-    isBlocking,
-    isUnblocking,
+    isblocking,
+    isunblocking
+
   };
 }
