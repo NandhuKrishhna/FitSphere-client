e4b0fa5233a841739fa6ad38eedf32771987afae
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-23 22:40:30 +0530

    fix: update code structure for improved readability and maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -8,26 +8,32 @@ import { PersistGate } from "redux-persist/integration/react";
 import { StrictMode } from "react";
 import { connectSocket } from "./lib/socketManager.ts";
 import CustomToaster from "./components/CustomToaster.tsx";
-persistor.subscribe(() => {
+import { ToastProvider } from "./components/ui/toast-container.tsx";
+const unsubscribe = persistor.subscribe(() => {
   if (persistor.getState().bootstrapped) {
     const state: RootState = store.getState();
     const { auth } = state;
-    console.log(auth)
+    console.log(auth);
 
     if (auth?.currentUser?._id) {
       connectSocket(auth.currentUser._id, store.dispatch);
     }
+
+    unsubscribe();
   }
 });
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <CustomToaster />
-          <Routes>
-            <Route path="/*" element={<App />} />
-          </Routes>
+          <ToastProvider>
+            <CustomToaster />
+            <Routes>
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </ToastProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
