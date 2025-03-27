b5a845c9181636413ab9904d4cb51be739e1141c
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-03-28 02:08:10 +0530

    feat(paymentUtils): enhance payment response types for better error handling and success verification

--- src/utils/paymentUtils.ts
+++ src/utils/paymentUtils.ts
@@ -1,7 +1,26 @@
 import { toast } from "react-hot-toast";
 import { RazorpayOrder } from "../types/Payments";
 
-export const initPay = (order: RazorpayOrder, onSuccess: (response: any) => void, onFailure: (error: any) => void) => {
+type PaymentFailedResponse = {
+  error: {
+    code: string;
+    description: string;
+    source: string;
+    step: string;
+    reason: string;
+    metadata: {
+      order_id?: string;
+      payment_id?: string;
+    };
+  };
+};
+export type PaymentSuccessResponse = {
+  razorpay_payment_id: string; // Unique ID of the payment
+  razorpay_order_id: string; // Order ID associated with the payment
+  razorpay_signature: string; // Signature used for verification
+};
+
+export const initPay = (order: RazorpayOrder, onSuccess: (response: unknown) => void, onFailure: (error: unknown) => void) => {
   console.log("Initializing payment with order:", order);
   if (!(window as any).Razorpay) {
     console.error("Razorpay SDK not loaded");
@@ -22,7 +41,7 @@ export const initPay = (order: RazorpayOrder, onSuccess: (response: any) => void
     name: "Appointment Payment",
     description: "Payment for consultation",
     order_id: order.id,
-    handler: function (response: any) {
+    handler: function (response: PaymentSuccessResponse) {
       console.log("Payment successful!", response);
       toast.success("Payment successful!");
       onSuccess(response);
@@ -41,7 +60,8 @@ export const initPay = (order: RazorpayOrder, onSuccess: (response: any) => void
 
   try {
     const rzp = new (window as any).Razorpay(options);
-    rzp.on("payment.failed", function (response: any) {
+    rzp.on("payment.failed", function (response: PaymentFailedResponse) {
+      console.log("Response for failing : ", response)
       console.error("Payment failed:", response.error);
       toast.error(`Payment failed: ${response.error.description}`);
       onFailure(response.error);
