cec3ac6e1c489dbbf1c22348479824285270d442
Nandhu Krishna <166598760+NandhuKrishhna@users.noreply.github.com>
2025-04-02 05:32:34 +0530

    feat: implement lazy loading for LandingPage with a loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/Users/LandingPage";
 import Layout from "./components/Layout";
 import Doctor_Routes from "./routes/Doctor_Routes";
 import Admin_Routes from "./routes/Admin_Routes";
@@ -7,13 +6,21 @@ import UserRoutes from "./routes/UserRoutes";
 import Unauthorized from "./pages/UnAuthorized";
 import GlobalSocketListener from "./components/App/GlobalSocketListener";
 import NotFound from "./pages/Not-Found";
+import { lazy, Suspense } from "react";
+import { Loader } from "lucide-react";
+
+const LandingPage = lazy(() => import("./pages/Users/LandingPage"));
 function App() {
   return (
     <>
       <GlobalSocketListener />
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<LandingPage />} />
+          <Route index element={<Suspense fallback={
+            <div className="h-screen bg-black flex items-center justify-center">
+              <Loader className="animate-spin text-gray-600 w-10 h-10" />
+            </div>
+          }><LandingPage /></Suspense>} />
           <Route path="/unauthorized" element={<Unauthorized />} />
           <Route path="/*" element={<UserRoutes />} />
           <Route path="/doctor/*" element={<Doctor_Routes />} />
